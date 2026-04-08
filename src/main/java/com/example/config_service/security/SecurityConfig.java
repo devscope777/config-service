de74b8629780f20bd739474d678975b73db42a3e
devscope777 <devscope777@gmail.com>
2026-04-08 19:52:01 -0400

    Add monitoring and observability.

--- src/main/java/com/example/config_service/security/SecurityConfig.java
+++ src/main/java/com/example/config_service/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(
-                        req -> req.requestMatchers("/actuator/health").permitAll().anyRequest().authenticated())
+                        req -> req.requestMatchers("/actuator/**").permitAll().anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
